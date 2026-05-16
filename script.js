74ee902314a06664b360a430c18ddab324e45b75
k881-droid <k881@newschool.edu>
2026-05-16 19:36:38 -0400

    Created square animation using js intersection observer

---
+++ script.js
@@ -0,0 +1,16 @@
+const expandComp = document.getElementById('expand-comp');
+
+const expandObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+      } else {
+        entry.target.classList.remove('visible');
+      }
+    });
+  },
+  { threshold: 0.3 }
+);
+
+expandObserver.observe(expandComp);
